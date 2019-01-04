651b0985bb83ce2316f46da4d88bef8bd6592c0c
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-04 07:02:44 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -25,7 +25,7 @@ describe 'form' do
   it 'shows a new form that submits content and redirects and prints out params' do
     visit new_post_path
 
-    fill_in 'post_title', with: "My post title"
+    fill_in 'title', with: "My post title"
     fill_in 'post_description', with: "My post description"
 
     click_on "Submit Post"
@@ -38,7 +38,7 @@ describe 'form' do
 
     visit edit_post_path(@post)
 
-    fill_in 'post_title', with: "My edit"
+    fill_in 'title', with: "My edit"
     fill_in 'post_description', with: "My post description"
 
     click_on "Submit Post"
